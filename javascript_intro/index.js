7c18e7704c1b432a76c55d4a64c965164fbe6969
joker-jonesy <spashmcgrath94@yahoo.com>
2023-12-05 11:34:34 -0500

    javascript demos

---
+++ javascript_intro/index.js
@@ -0,0 +1,29 @@
+// console.log("hello")
+
+// numbers in js
+const peanutNumber = 10;
+let milkGallons = 3
+milkGallons = milkGallons+3;
+// console.log(peanutNumber/milkGallons);
+
+// strings
+
+const firstName = 'Luke';
+const lastName ="Jones";
+const fullName = firstName +" "+ lastName;
+// console.log(fullName)
+
+// numbers in strings
+
+const num1 = Number("12");
+const num2 = Number("36");
+const sum = num1+num2;
+console.log("Your final result is: "+sum);
+
+let sayHello = prompt("WHat is your name?");
+console.log(sayHello+" says hello");
+
+// prompts return strings
+
+
+
